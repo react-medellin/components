37ccdaed84603596bced482528922c780eb41f67
Alejandro Ñáñez Ortiz <alejonanez@gmail.com>
2017-05-27 16:41:49 -0500

    Styled components (#2)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
+import {
+	seaSerpent,
+	white,
+	tealBlue,
+	maximumBlue,
+	smallFont,
+	mediumFont,
+	bigFont
+} from '../../constants';
 
-export const Button = () => (
-	<button>I am a dumb component</button>
-);
+const fontSizes = {
+	sm: smallFont,
+	md: mediumFont,
+	lg: bigFont,
+	default: mediumFont
+};
+
+const ANIMATION_TIME = '500ms';
+
+const getButtonSize = props => fontSizes[props.size] || fontSizes.default;
+
+// Base Button Styles
+const ButtonStyles = styled.button`
+	border-radius: .1em;
+	padding: .5em 1em;
+	outline: none;
+	font-size: ${props => getButtonSize(props)};
+	transition: background-color ${ANIMATION_TIME} ease,
+				color ${ANIMATION_TIME} ease,
+				border ${ANIMATION_TIME} ease;
+
+	:hover {
+		cursor: pointer;
+	}
+`;
+
+// Primary Button Styles
+const PrimaryButton = ButtonStyles.extend`
+	background: ${seaSerpent};
+	color: ${white};
+	border: 1px solid ${white};
+
+	:hover {
+		background: ${tealBlue};
+		border: 1px solid ${tealBlue};
+		color: ${white};
+	}
+`;
+
+// Default Button Styles
+const SecondaryButton = ButtonStyles.extend`
+	background: ${white};
+	color: ${seaSerpent};
+	border: 1px solid ${maximumBlue};
+
+	:hover {
+		border: 1px solid ${tealBlue};
+		color: ${tealBlue};
+	}
+`;
+
+const buttonTypes = {
+	primary: PrimaryButton,
+	secondary: SecondaryButton,
+	default: PrimaryButton
+};
+
+const getButtonType = props => buttonTypes[props.type] || buttonTypes.default;
+
+export const Button = (props) => {
+	const Btn = getButtonType(props);
+
+	return (
+		<Btn {...props}>{props.children}</Btn>
+	);
+};
---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+// Colors
+export const seaSerpent = '#54c1de';
+export const white = '#ffffff';
+export const tealBlue = '#367b8e';
+export const maximumBlue = '#4aa9c4';
+export const queenPink = '#e7d5dd';
+export const rocketMetallic = '#8b7981';
+export const teaGreen = '#ceffc8';
+export const camouflageGreen = '#718c6e';
+export const cornSkin = '#fcf8d9';
+export const nikel = '#737163';
+
+// Fonts
+export const smallFont = '0.8em';
+export const mediumFont = '1em';
+export const bigFont = '1.2em';
