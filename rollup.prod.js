9ee95865afb91096d2fc8c9848e80fdcb6f124bb
Alejandro Ñáñez Ortiz <alejonanez@gmail.com>
2017-05-31 19:18:29 -0500

    Improve build (#7)
    * Create separated prod and dev builds + updated npm scripts
    
    * Update package name + minor updates on build process

---
+++ rollup.prod.js
@@ -0,0 +1,51 @@
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+const uglify = require('rollup-plugin-uglify');
+const flow = require('rollup-plugin-flow');
+const replace = require('rollup-plugin-replace');
+const { minify } = require('uglify-es');
+const pkg = require('./package.json');
+
+const plugins = [
+	resolve(),
+	babel({
+		exclude: 'node_modules/**'
+	}),
+	flow(),
+	replace({
+		'process.env.NODE_ENV': JSON.stringify('production')
+	}),
+	commonjs(),
+	// Since we're using ES modules we need this _hack_
+	// https://github.com/TrySound/rollup-plugin-uglify#warning
+	uglify({}, minify)
+];
+
+const config = {
+	banner: `/* react-med-lib version ${pkg.version} */`,
+	footer: '/* Join our community! https://meetup.com/React-Medellin */',
+	entry: 'src/index.js',
+	moduleName: 'ReactMedLib',
+	targets: [
+		{
+			dest: pkg.main,
+			format: 'umd',
+			sourceMap: true
+		},
+		{
+			dest: pkg.module,
+			format: 'es',
+			sourceMap: true
+		}
+	],
+	external: [
+		'react'
+	],
+	globals: {
+		react: 'React'
+	},
+	plugins
+};
+
+module.exports = config;
