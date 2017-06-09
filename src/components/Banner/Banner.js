63d2c7b24ed68fa5b8deaeec582e75277827844f
Alejandro Nanez <alejonanez@gmail.com>
2017-06-09 18:07:40 -0500

    Adding === into banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -52,6 +52,6 @@ export const Banner = (props) => {
 	const BannerType = bannerType[type] || bannerType.default;
 
 	return (
-		<BannerType {...props}>doiasjdioasjdasiodjad</BannerType>
+		<BannerType {...props}>====={props.children}=====</BannerType>
 	);
 };
