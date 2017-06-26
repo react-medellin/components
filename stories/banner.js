ee402ec41787ff292990233f678c3b0794cb70a5
Alejandro Nanez <alejonanez@gmail.com>
2017-06-26 15:51:21 -0500

    Updated banner story

--- stories/banner.js
+++ stories/banner.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { Banner } from '../dist/index';
 
-storiesOf('Button', module)
-	.add('with text', () => (
-		<Banner onClick={action('clicked')}>Hello Banner</Banner>
-	)).add('with some emoji', () => (
-		<Banner onClick={action('clicked')}>😀 😎 👍 💯</Banner>
-	));
+const Wrapper = ({ children }) => (
+	<div
+		style={{
+			padding: '3em'
+		}}
+	>
+		{children}
+	</div>
+);
 
+storiesOf('Banner', module)
+	.add('Success Banner', () => (
+		<Wrapper>
+			<Banner type="success">Success banner</Banner>
+		</Wrapper>
+	))
+	.add('Warning Banner', () => (
+		<Wrapper>
+			<Banner type="warning">Warning banner</Banner>
+		</Wrapper>
+	))
+	.add('Error Banner', () => (
+		<Wrapper>
+			<Banner type="error">Error banner</Banner>
+		</Wrapper>
+	));
